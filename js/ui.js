a6adb8c042731cb2b74480a44f369e0739f3ac45
MTlpc <33110794+MTlpc@users.noreply.github.com>
2017-10-29 16:55:34 +0800

    banner slider

--- js/ui.js
+++ js/ui.js
@@ -60,6 +60,75 @@ $.fn.UiBacktop = function () {
 });
 }
 
+//ui-slider
+
+$.fn.UiSlider = function () {
+
+    var ui = $(this);
+
+    var wrap = $('.ui-slider-wrap');
+
+    var btn_prev = $('.ui-slider-arrow .left',ui);
+    var btn_next = $('.ui-slider-arrow .right',ui);
+
+    var items = $('.ui-slider-wrap .item',ui);
+    var tips = $('.ui-slider-process .item',ui);
+
+    var current = 0;
+    var size = items.length;
+    var width = items.eq(0).width();
+    var enableAuto = true;
+    
+    wrap
+        .on('mouseover',function () {
+            enableAuto = false;
+        })
+        .on('mouseout',function () {
+            enableAuto = true;
+        });
+
+    //具体操作
+    wrap.on('move_prev',function () {
+        if(current <= 0){
+            current = size;
+        }
+        current = current -1 ;
+
+        wrap.triggerHandler('move_to',current);
+    })
+        .on('move_next',function () {
+            if (current >= size - 1){
+                current = -1;
+            }
+            current = current +1;
+            wrap.triggerHandler('move_to',current);
+        })
+        .on('move_to',function (evt,index) {
+            wrap.css('left',index*width*-1);
+            tips.removeClass('item_focus').eq(index).addClass('item_focus');
+        })
+        .on('auto_move',function () {
+            setInterval(function () {
+                enableAuto && wrap.triggerHandler('move_next');
+            },2000);
+        })
+        .triggerHandler('auto_move');
+
+    //事件
+    btn_prev.on('click',function () {
+        wrap.triggerHandler('move_prev');
+    });
+
+    btn_next.on('click',function () {
+        wrap.triggerHandler('move_next');
+    });
+
+    tips.on('click',function () {
+        var index = $(this).index();
+        wrap.triggerHandler('move_to',index);
+    })
+}
+
 //页面的脚本逻辑
 
 $(function () {
@@ -67,4 +136,6 @@ $(function () {
     $('.content-tab').UiTab('.caption > .item','.block > .item','');
     $('.content-tab .block .item').UiTab('.block-caption > a','.block-content > .block-wrap','block-caption-');
     $('body').UiBacktop();
+
+    $('.ui-slider').UiSlider();
 });
